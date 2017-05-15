ef14dd1d4b4d9f96cff33b59628dc4dc3267a3b5
Rahul Deshpande <Rahul Deshpande>
2017-05-15 11:51:04 +0530

    New build to fix rmse

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -65,6 +65,13 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   VectorXd z_pred(3);
   z_pred << rho, phi, rho_dot;
   VectorXd y = z - z_pred;
+
+  //Normalizing y(1) so phi is between -pi and pi
+
+  while (y(1) < -M_PI)
+    y(1) += 2 * M_PI;
+  while (y(1) > M_PI)
+    y(1) -= 2 * M_PI;
   MatrixXd Ht = H_.transpose();
   MatrixXd S = H_ * P_ * Ht + R_;
   MatrixXd Si = S.inverse();
